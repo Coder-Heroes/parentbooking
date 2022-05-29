e8497398bb592d68aa4fb13646b5effc5f3028be
RFNoC <rj.geng@gmail.com>
2022-05-29 15:44:05 -0700

    Added router and swith for index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter as Router,
+  Route,
+  useHistory,
+  Switch,
+} from 'react-router-dom';
 
 import './index.css';
 import { Layout } from 'antd';
+import { applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-// import App from './App';
 import ParentBooking from './components/pages/ParentBooking';
 
 ReactDOM.render(
-  <React.StrictMode>     
-    <Layout>
-      {/* <App /> */}
-      <ParentBooking />
-    </Layout>    
-  </React.StrictMode>,
+  <Router>
+    <React.StrictMode>     
+      <Layout>  
+        <App />            
+      </Layout>    
+    </React.StrictMode>
+  </Router>,
   document.getElementById('root')
 );
+
+function App() { 
+  const history = useHistory();
+  return (
+    <Layout.Content style={{ display: 'flex', justifyContent: 'center' }}>
+        <Switch>
+        <Route path="/parent/booking" component={ParentBooking} />    
+        </Switch>
+    </Layout.Content>
+  )  
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
